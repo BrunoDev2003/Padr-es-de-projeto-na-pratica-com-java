799af62886dfae4a91c1db72c464ad6da6c9d73f
Bruno Gusmão <brunogusmao43@gmail.com>
2022-08-18 00:29:00 -0300

    terminando o projeto com as operações Crud no client service

---
+++ lab-padroes-projeto-java/projeto-com-spring-initializer/demo/demo/src/service/clientService.java
@@ -0,0 +1,14 @@
+
+
+
+public class clientService {
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id,Cliente cliente);
+
+    void deletar(Long id);
+}
